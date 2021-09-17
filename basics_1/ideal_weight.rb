85a48ceb52e3e925687f9442dc42915b0c148aa1
NeskladAnya <belko.hanna.off@gmail.com>
2021-09-17 08:53:03 +0200

    Fix tabulation again

--- basics_1/ideal_weight.rb
+++ basics_1/ideal_weight.rb
@@ -7,7 +7,7 @@ height = gets.chomp.to_i
 ideal_weight = (height - 110) * 1.15
 
 if ideal_weight < 0
-	puts "Hi, #{name}! Your weight is ideal!"
+  puts "Hi, #{name}! Your weight is ideal!"
 else
-	puts "Hi, #{name}! Your ideal weight is #{ideal_weight.to_i} kg!"
+  puts "Hi, #{name}! Your ideal weight is #{ideal_weight.to_i} kg!"
 end
