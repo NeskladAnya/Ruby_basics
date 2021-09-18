86b7996693e32de91db25647846405c6dae558a4
NeskladAnya <belko.hanna.off@gmail.com>
2021-09-18 16:20:30 +0200

    Task 6

---
+++ basics_2/task_6.rb
@@ -0,0 +1,27 @@
+goods = Hash.new
+
+loop do
+  puts "Введите название товара:"
+  item = gets.chomp.downcase
+
+  break if item == "стоп"
+  
+  puts "Введите цену за единицу товара:"
+  price = gets.chomp.to_f
+
+  puts "Введите количество единиц товара:"
+  quantity = gets.chomp.to_f
+
+  goods[item] = { price: price, quantity: quantity}
+end
+
+total_price = 0
+
+goods.each do |key, value|
+  puts "Товар: #{key}. Цена за единицу: #{value[:price]}, количество: #{value[:quantity]}"
+  item_price = value[:price] * value[:quantity]
+  puts "Стоимость товара: #{item_price}"
+  total_price += item_price
+end
+
+puts "Стоимость всех товаров: #{total_price}"
